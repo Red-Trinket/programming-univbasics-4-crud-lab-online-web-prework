c31967583cdcf3acaa622150001e0daca45499e8
Matthew Jones <matts3371@gmail.com>
2019-09-04 21:36:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,38 +3,37 @@ def create_an_empty_array
 end
 
 def create_an_array
-  real_array = ["entry 1," "entry 2," "entry 3," "entry 4"]
+  array = ["entry 1," "entry 2," "entry 3," "entry 4"]
 end
 
 def add_element_to_end_of_array(array, element)
-  real_array.push["pushed entry"]
+  array.push["pushed entry"]
 end
 
 def add_element_to_start_of_array(array, element)
-  real_array.unshift["unshift entry"]
+  array.unshift["unshift entry"]
 end
 
 def remove_element_from_end_of_array(array)
-  real_array.pop 
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  real_array.shift 
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  A = real_array[1]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  B = real_array[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  C = real_array.length - 1
-  D = real_array[C]
+  array[array.length - 1]
 end
 
 def update_element_from_index(array, index_number, element)
-  real_array[0] = "new element"
+  array[index_number] = element
 end
