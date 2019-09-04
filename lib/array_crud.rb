8da4d6ce0f6209b247b5aacc98b2621c7c8168e1
Matthew Jones <matts3371@gmail.com>
2019-09-04 21:32:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  real_array = ["entry"]
+  real_array = ["entry 1," "entry 2," "entry 3," "entry 4"]
 end
 
 def add_element_to_end_of_array(array, element)
