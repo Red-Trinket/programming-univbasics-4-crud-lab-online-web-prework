b0736e46ffefa281a36855b41bc2b8eed0041353
Matthew Jones <matts3371@gmail.com>
2019-09-04 21:37:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push["pushed entry"]
+  array.push[element]
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift["unshift entry"]
+  array.unshift[element]
 end
 
 def remove_element_from_end_of_array(array)
